a10dfb299b800c48892ba28962186ba5301975e7
Zharas <231237@astanait.edu.kz>
2023-12-27 20:11:50 +0600

    FInal Assignment 1

--- src/MyApplication.java
+++ src/MyApplication.java
@@ -1,37 +1,41 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MyApplication {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+        String filePath = "C:\\Users\\acer\\IdeaProjects\\Zhass\\src\\coordinates";
+        try {
+            List<Point> pointsList = readPointsFromFile(filePath);
 
-        List<Point> pointsList = readPointsFromFile();
-
-
-        Point[] points = pointsList.toArray(new Point[0]);
-
-
-        Shape shape = new Shape(points);
+            Point[] points = pointsList.toArray(new Point[0]);
 
 
-        System.out.println("Perimeter: " + shape.perimeter());
-        System.out.println("longestSide: " + shape.longestSide());
-        System.out.println("averageSide: " + shape.averageSide());
+            Shape shape = new Shape(points);
+
+
+            System.out.println("perimeter: " + shape.perimeter());
+            System.out.println("longestSide: " + shape.longestSide());
+            System.out.println("averageSide: " + shape.averageSide());
+        } catch (IOException e) {
+            System.err.println("error file: " + e.getMessage());
+        }
     }
 
-    private static List<Point> readPointsFromFile() throws IOException {
+    private static List<Point> readPointsFromFile(String filePath) throws IOException {
         List<Point> points = new ArrayList<>();
 
-        try (BufferedReader reader = new BufferedReader(new FileReader("text.txt"))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] coordinates = line.split(" ");
-                double x = Double.parseDouble(coordinates[0]);
-                double y = Double.parseDouble(coordinates[1]);
-                points.add(new Point(x, y));
-            }
+        Path path = Paths.get(filePath);
+        List<String> lines = Files.readAllLines(path);
+
+        for (String line : lines) {
+            String[] coordinates = line.split(" ");
+            double x = Double.parseDouble(coordinates[0]);
+            double y = Double.parseDouble(coordinates[1]);
+            points.add(new Point(x, y));
         }
 
         return points;
